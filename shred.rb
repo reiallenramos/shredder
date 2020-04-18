03455127798c02f11379516c20eaea7e8f3770f8
Rei Allen Ramos <reiallenramos@gmail.com>
2020-04-18 22:56:35 +0800

    1 complete iteration

--- shred.rb
+++ shred.rb
@@ -1,5 +1,11 @@
 require 'rmagick'
+require 'fileutils'
 include Magick
+
+# usage:
+# ruby shred.rb <path_to_file> <no of iterations>
+# example:
+# ruby shred.rb ./images/my_image.jpg 1
 
 class Shredder
   attr_accessor :input, :pieces, :input_width, :input_height, :thickness
@@ -9,13 +15,14 @@ class Shredder
     @input_height = @input.rows
   end
 
-  def shred(pieces)
+  def shred(image, pieces)
     @pieces = pieces
     @thickness = @input_width / @pieces
 
     shredded_pieces = ImageList.new
     @pieces.times do |index|
-      pixels = get_pixels(index * @thickness)
+      x_offset = index * @thickness
+      pixels = get_pixels(image, x_offset)
       generated = Image.constitute(@thickness,@input_height,'RGB', pixels)
       shredded_pieces << generated
     end
@@ -23,16 +30,51 @@ class Shredder
     shredded_pieces
   end
 
-  private
+  def stitch(shredded_pieces)
+    odd = ImageList.new()
+    even = ImageList.new()
 
-  def get_pixels(x_offset)
-    input.dispatch(x_offset,0,thickness,input_height,'RGB')
+    shredded_pieces.each_with_index do |piece, index|
+      if index.odd?
+        odd << piece
+      else
+        even << piece
+      end
+    end
+
+    new_array = even.concat(odd)
+
+    new_array.each_with_index do |piece, index|
+      piece.page = Rectangle.new(thickness, input_height, index * thickness, 0)
+      piece.page
+    end
+
+    new_array.mosaic
   end
 
+  private
+
+  def get_pixels(image, x_offset)
+    image.dispatch(x_offset,0,thickness,input_height,'RGB')
+  end
 end
+
 input_image = Image.read(ARGV[0]).first
-shred = Shredder.new(input_image)
+iterations = ARGV[1].to_i
+columns = 100
+iterations.times do |index|
+  shred = Shredder.new(input_image)
+  first_shred_pieces = shred.shred(input_image, columns)
+  first_stitch = shred.stitch(first_shred_pieces)
+  rotated_first_stitch = first_stitch.rotate(90)
+  second_shred_pieces = shred.shred(rotated_first_stitch, columns)
+  second_stitch = shred.stitch(second_shred_pieces)
 
-shredded_pieces = shred.shred(10)
+  final = second_stitch.rotate(-90)
+  filename = input_image.filename.split('/').last
+  new_filename = filename.split('.')[0]
+  final.write("#{new_filename}_#{iterations + index}_iterations.jpg")
+end
 
+puts 'done!'
 exit
