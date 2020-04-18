94fc4d210cebecd5326738d01c879be0494f3310
Rei Allen Ramos <reiallenramos@gmail.com>
2020-04-18 21:04:28 +0800

    initial commit: basic shredder

---
+++ shred.rb
@@ -0,0 +1,39 @@
+require 'rmagick'
+include Magick
+
+class Shredder
+  attr_accessor :input, :pieces, :input_width, :input_height, :thickness
+  def initialize(input)
+    @input = input
+    @input_width = @input.columns
+    @input_height = @input.rows
+  end
+
+  def shred(pieces)
+    @pieces = pieces
+    @thickness = @input_width / @pieces
+
+    shredded_pieces = ImageList.new
+    @pieces.times do |index|
+      pixels = get_pixels(index * @thickness)
+      generated = Image.constitute(@thickness,@input_height,'RGB', pixels)
+      shredded_pieces << generated
+    end
+
+    shredded_pieces
+  end
+
+  private
+
+  def get_pixels(x_offset)
+    input.dispatch(x_offset,0,thickness,input_height,'RGB')
+  end
+
+end
+
+input_image = Image.read('rap.jpg').first
+shred = Shredder.new(input_image)
+
+shredded_pieces = shred.shred(10)
+
+exit
